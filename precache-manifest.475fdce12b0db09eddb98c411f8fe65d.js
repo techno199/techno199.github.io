1904575cc2c97904d5baf54970079c0f2468e2ae
ksan199 <techno199@live.ru>
2019-04-03 12:53:27 +0300

    funbox proj upload

---
+++ precache-manifest.475fdce12b0db09eddb98c411f8fe65d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88cf06b57df31bb0affb",
+    "url": "/static/js/main.8791a61f.chunk.js"
+  },
+  {
+    "revision": "48c19c244e2366750590",
+    "url": "/static/js/2.9dc17ac8.chunk.js"
+  },
+  {
+    "revision": "88cf06b57df31bb0affb",
+    "url": "/static/css/main.b2b01187.chunk.css"
+  },
+  {
+    "revision": "508c058e6fbec01c01abee8cef1adaeb",
+    "url": "/index.html"
+  }
+];
